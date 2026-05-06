09364175684fbdaff4dede0730d9505d5332441d
Colby Gilbert <colby@firmware.ai>
2026-05-06 11:59:14 -0700

    fix: generate import map

--- dev/app/(payload)/admin/importMap.js
+++ dev/app/(payload)/admin/importMap.js
@@ -1,5 +1,8 @@
+import { HlsVideoField as HlsVideoField_4fa5e52255bf9b3cd93035bc02859612 } from 'plugin-adaptive-bitrate-videos/client'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
-
+/** @type import('payload').ImportMap */
 export const importMap = {
-
+  "plugin-adaptive-bitrate-videos/client#HlsVideoField": HlsVideoField_4fa5e52255bf9b3cd93035bc02859612,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
